e7af32923d9eea1ba62efc9e6bba34cbf97648d3
Tom Biebl <tom.biebl@gmx.de>
2026-05-02 16:47:09 +0200

    test sheet

--- scripts/loa-system.mjs
+++ scripts/loa-system.mjs
@@ -1,26 +1,33 @@
 class LoAActorSheet extends ActorSheet {
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["loa-system", "sheet", "actor"],
-      template: "systems/loa-system/templates/actor-sheet.hbs",
-      width: 500,
-      height: 500
-    });
-  }
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            classes: ["loa-system", "sheet", "actor"],
+            template: "systems/loa-system/templates/actor-sheet.hbs",
+            width: 920,
+            height: 720,
+            tabs: [
+                {
+                    navSelector: ".loa-tabs",
+                    contentSelector: ".loa-content",
+                    initial: "main"
+                }
+            ]
+        });
+    }
 
-  getData(options) {
-    const data = super.getData(options);
-    data.system = this.actor.system;
-    return data;
-  }
+    getData(options) {
+        const data = super.getData(options);
+        data.system = this.actor.system;
+        return data;
+    }
 }
 
 Hooks.once("init", () => {
-  console.log("LoA System | Initializing");
+    console.log("LoA System | Initializing");
 
-  Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("loa-system", LoAActorSheet, {
-    types: ["character", "npc"],
-    makeDefault: true
-  });
+    Actors.unregisterSheet("core", ActorSheet);
+    Actors.registerSheet("loa-system", LoAActorSheet, {
+        types: ["character", "npc"],
+        makeDefault: true
+    });
 });
